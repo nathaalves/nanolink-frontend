abab290159bc695208c944caa9cadbcd1528b405
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-11 23:31:08 -0300

    feat: create Main template

---
+++ src/templates/Main.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { quicksand } from '@/assets/fonts/quicksand';
+import Image from 'next/image';
+import mainIllustration from '@/assets/images/main-illustration.svg';
+import { ResponsiveContainer } from '@/components/ResponsiveContainer';
+import { LinkHandler } from '@/components/LinkHandler';
+
+export function Main() {
+  return (
+    <ResponsiveContainer className="flex flex-col flex-1 items-center mx-auto md:flex-row">
+      <div className="md:min-w-[500px]">
+        <Image src={mainIllustration} alt="Main illustration" />
+      </div>
+      <div className="flex flex-col w-full p-4 gap-4 md:order-first md:min-w-[28rem] lg:min-w-[36rem] bg-white">
+        <h1 className={'text-3xl text-center ' + quicksand.className}>
+          Não perca mais tempo com links gigantes.{' '}
+          <span className="text-sky-600">Encurte já!</span>
+        </h1>
+        <p
+          className={'text-xl text-center text-gray-500 ' + quicksand.className}
+        >
+          Faça seu site brilhar com links curtos e atraentes.{' '}
+          <span className="hidden">
+            Monitore suas estatísticas em tempo real e
+          </span>
+          Impulsione seu sucesso na web!
+        </p>
+        <LinkHandler />
+      </div>
+    </ResponsiveContainer>
+  );
+}
