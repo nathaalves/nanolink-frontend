a4ffd2a0994b6cd63ebf2bd2e0ed1fd451d335a9
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-11 23:29:07 -0300

    feat: create SandwichButton component

---
+++ src/components/SandwichButton.tsx
@@ -0,0 +1,9 @@
+export function SandwichButton() {
+  return (
+    <button className="hiden flex-col justify-between w-8 h-8 p-1">
+      <span className="w-full h-1 bg-sky-600 rounded-full"></span>
+      <span className="w-full h-1 bg-sky-600 rounded-full"></span>
+      <span className="w-full h-1 bg-sky-600 rounded-full"></span>
+    </button>
+  );
+}
