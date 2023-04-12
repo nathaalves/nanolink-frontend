c34ca44f56bd168a51827ece771ba1867536d027
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-11 23:30:50 -0300

    feat: create Header template

---
+++ src/templates/Header.tsx
@@ -0,0 +1,19 @@
+import { ResponsiveContainer } from '@/components/ResponsiveContainer';
+import { SandwichButton } from '@/components/SandwichButton';
+import { sriracha } from '@/assets/fonts/sriracha';
+import Link from 'next/link';
+
+export function Header() {
+  return (
+    <header className="flex items-center justify-center bg-white h-24">
+      <ResponsiveContainer className="flex items-center justify-between p-4">
+        <Link href="https://nanolink.app.br">
+          <h1 className={'text-4xl text-sky-600 ' + sriracha.className}>
+            Nano Link
+          </h1>
+        </Link>
+        <SandwichButton />
+      </ResponsiveContainer>
+    </header>
+  );
+}
