ba9183b3e3388c4a3568d1c9183555e6dbf7b25d
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-15 11:36:30 -0300

    feat: remove font class name from input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,3 @@
-import { quicksand } from '@/assets/fonts/quicksand';
 import { InputHTMLAttributes } from 'react';
 
 type InputProps = InputHTMLAttributes<HTMLInputElement>;
@@ -6,10 +5,7 @@ type InputProps = InputHTMLAttributes<HTMLInputElement>;
 export function Input({ ...rest }: InputProps) {
   return (
     <input
-      className={
-        'h-12 border rounded pl-4 focus:outline-sky-600 truncate ' +
-        quicksand.className
-      }
+      className={'h-12 border rounded pl-4 focus:outline-sky-600 truncate'}
       {...rest}
     />
   );
