844d454080bd13cfb047931f1db333135cded9e9
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-15 11:35:21 -0300

    feat: add load spinner to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,25 @@
-import { quicksand } from '@/assets/fonts/quicksand';
 import { ButtonHTMLAttributes } from 'react';
+import { LoadSpinner } from './LoadSpinner';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-export function Button({ children, ...rest }: ButtonProps) {
+export function Button({ children, disabled, ...rest }: ButtonProps) {
   return (
     <button
-      className={
-        'h-12 rounded bg-sky-600 text-white font-bold px-4 ' +
-        quicksand.className
-      }
+      className={`
+        flex items-center
+        justify-center
+        h-12
+        rounded
+        bg-sky-600
+        text-white
+        font-bold 
+        px-4 py-2
+        ${disabled ? 'opacity-50 ' : 'hover:opacity-75 '}
+     `}
       {...rest}
     >
-      {children}
+      {disabled ? <LoadSpinner /> : children}
     </button>
   );
 }
