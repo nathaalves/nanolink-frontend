8c479d0b6a8504440f0dd7488249b1b521dcc325
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-15 12:00:00 -0300

    refactor: refactor LinkHandler component

--- src/components/LinkHandler.tsx
+++ src/components/LinkHandler.tsx
@@ -1,25 +1,20 @@
 'use client';
 
-import { Button } from '@/components/Button';
-import { useState } from 'react';
-import { shortLinkService } from '@/services/shortLinkService';
 import Link from 'next/link';
+import { useState } from 'react';
+import { Button } from '@/components/Button';
 import { Input } from './Input';
-import { quicksand } from '@/assets/fonts/quicksand';
+import { ErrorMessage } from './ErrorMessage';
+import { useCreateShortLinkMutation } from '@/hooks/mutations/useCreateShortLinkMutation';
 
 export function LinkHandler() {
   const [originalURL, setOriginalURL] = useState('');
-  const [shortLink, setShortLink] = useState<string | null>(null);
-  const [url, setUrl] = useState<string | null>(null);
 
-  const createShortLink = async () => {
-    const { url, shortLink } = await shortLinkService.get(originalURL);
-    setShortLink(shortLink);
-    setUrl(url);
-  };
+  const { data, mutate, isLoading, isSuccess, errorData, isError } =
+    useCreateShortLinkMutation(originalURL);
 
   const copyText = async () => {
-    await navigator.clipboard.writeText(shortLink as string);
+    await navigator.clipboard.writeText(data?.shortLink ?? '');
   };
 
   return (
@@ -29,25 +24,27 @@ export function LinkHandler() {
         value={originalURL}
         onChange={(event) => setOriginalURL(event.target.value)}
       />
-      <Button onClick={createShortLink}>Criar URL</Button>
-      {shortLink && (
+      {isError && (
+        <div>
+          {errorData?.details.map((detail, index) => (
+            <ErrorMessage key={index}>{detail.message}</ErrorMessage>
+          ))}
+        </div>
+      )}
+      <Button disabled={isLoading} onClick={() => mutate()}>
+        Criar URL
+      </Button>
+      {isSuccess && (
         <>
           <hr className="border-red-500" />
           <div className="flex items-center gap-4">
             <div className="flex-1 min-w-0">
-              <Link href={`https://${shortLink}`} target="_blank">
-                <strong
-                  className={
-                    'text-sky-600 hover:text-black truncate ' +
-                    quicksand.className
-                  }
-                >
-                  {shortLink}
+              <Link href={`https://${data?.shortLink}`} target="_blank">
+                <strong className={'text-sky-600 hover:text-sky-700 truncate'}>
+                  {data?.shortLink}
                 </strong>
               </Link>
-              <p className={'truncate text-gray-500 ' + quicksand.className}>
-                {url}
-              </p>
+              <p className={'truncate text-gray-500'}>{data?.url}</p>
             </div>
             <Button onClick={copyText}>Copiar</Button>
           </div>
