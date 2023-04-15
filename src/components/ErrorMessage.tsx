f42ef3b24689a36e216ba187c5e757cb6d1280a2
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-15 11:39:58 -0300

    feat: create ErrorMessage component

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,14 @@
+import { MdError } from 'react-icons/md';
+
+type ErroMessagePropsType = {
+  children: string;
+};
+
+export function ErrorMessage({ children }: ErroMessagePropsType) {
+  return (
+    <div className="flex items-center gap-2">
+      <MdError className="text-red-600 text-xl" />
+      <p className={'text-red-600'}>{children}</p>
+    </div>
+  );
+}
