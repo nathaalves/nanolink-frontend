1ede9755916c73625346b2b45c3cba8ae9788bc3
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-17 12:12:44 -0300

    style: turn image responsive

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -7,9 +7,9 @@ import { LinkHandler } from '@/components/LinkHandler';
 
 export function Main() {
   return (
-    <ResponsiveContainer className="flex flex-col flex-1 items-center mx-auto md:flex-row">
-      <div className="md:min-w-[500px]">
-        <Image src={mainIllustration} alt="Main illustration" />
+    <ResponsiveContainer className="flex flex-col flex-1 items-center md:flex-row">
+      <div className="sm:max-w-[500px] md:min-w-[700px] lg:min-w-[900px] md:translate-x-[-4rem]">
+        <Image src={mainIllustration} alt="Main illustration" priority />
       </div>
       <div className="flex flex-col w-full p-4 gap-4 md:order-first md:min-w-[28rem] lg:min-w-[36rem]">
         <h1 className={'text-3xl text-center'}>
