a5d0d41f1cb10bc318a715a17ced22fda606e754
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-04-17 15:41:52 -0300

    feat: disable button when input is empty

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,16 @@
 import { ButtonHTMLAttributes } from 'react';
 import { LoadSpinner } from './LoadSpinner';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isLoading?: boolean;
+};
 
-export function Button({ children, disabled, ...rest }: ButtonProps) {
+export function Button({
+  children,
+  disabled,
+  isLoading,
+  ...rest
+}: ButtonProps) {
   return (
     <button
       className={`
@@ -17,9 +24,10 @@ export function Button({ children, disabled, ...rest }: ButtonProps) {
         px-4 py-2
         ${disabled ? 'opacity-50 ' : 'hover:opacity-75 '}
      `}
+      disabled={disabled || isLoading}
       {...rest}
     >
-      {disabled ? <LoadSpinner /> : children}
+      {isLoading ? <LoadSpinner /> : children}
     </button>
   );
 }
