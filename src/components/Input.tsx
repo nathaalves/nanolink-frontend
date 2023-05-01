f916d832dd94035fd5900f0603a41e7b8f7faaef
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-05-01 18:50:03 -0300

    feat: create input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,77 @@
-import { InputHTMLAttributes } from 'react';
+'use client';
 
-type InputProps = InputHTMLAttributes<HTMLInputElement>;
+import { InputHTMLAttributes, useRef, useState } from 'react';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
+import { IoMdMail, IoMdLock } from 'react-icons/io';
+import { BsFillPersonFill } from 'react-icons/bs';
 
-export function Input({ ...rest }: InputProps) {
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  label?: string;
+  labelVisible?: boolean;
+  icon?: 'email' | 'password' | 'name';
+};
+
+export function Input({
+  label,
+  labelVisible = true,
+  type = 'text',
+  icon,
+  ...rest
+}: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
+  const icocnStyle = 'text-2xl text-zinc-600';
+
+  const icons = (name: string) => {
+    switch (name) {
+      case 'email':
+        return <IoMdMail className={icocnStyle} />;
+      case 'password':
+        return <IoMdLock className={icocnStyle} />;
+      case 'name':
+        return <BsFillPersonFill className={icocnStyle} />;
+    }
+  };
+
   return (
-    <input
-      className={'h-12 border rounded pl-4 focus:outline-sky-600 truncate'}
-      {...rest}
-    />
+    <div className="flex flex-col gap-1" onClick={handleClick}>
+      {labelVisible && (
+        <label htmlFor={rest.id} className="text-slate-700">
+          {label}
+        </label>
+      )}
+      <div className="flex items-center h-12 px-2 gap-2 border rounded bg-white focus-within:border-sky-600 relative">
+        {icon && icons(icon)}
+        <input
+          aria-label={label}
+          className={'flex-1 h-max outline-none truncate'}
+          type={type === 'password' ? (isVisible ? 'text' : 'password') : type}
+          ref={inputRef}
+          {...rest}
+        />
+        {type === 'password' && (
+          <>
+            {isVisible ? (
+              <AiFillEye
+                onClick={() => setIsVisible(!isVisible)}
+                className={`${icocnStyle} cursor-pointer`}
+              />
+            ) : (
+              <AiFillEyeInvisible
+                onClick={() => setIsVisible(!isVisible)}
+                className={`${icocnStyle} cursor-pointer`}
+              />
+            )}
+          </>
+        )}
+      </div>
+    </div>
   );
 }
