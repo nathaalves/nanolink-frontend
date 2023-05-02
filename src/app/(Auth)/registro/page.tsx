5864ddd16ea61449d54c61bdfbad612db597caa2
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-05-01 23:30:47 -0300

    feat: control button and input state

--- src/app/(Auth)/registro/page.tsx
+++ src/app/(Auth)/registro/page.tsx
@@ -3,6 +3,7 @@
 import { Button } from '@/components/Button';
 import { Form } from '@/components/Form';
 import { Input } from '@/components/Input';
+import { useSignupMutation } from '@/hooks/mutations/useSignupMutation';
 import { useForm } from '@/hooks/useForm';
 
 export default function Signup() {
@@ -10,14 +11,14 @@ export default function Signup() {
     name: '',
     email: '',
     password: '',
-    confirmPassword: '',
+    passwordConfirmation: '',
   });
 
-  const handleSubmit = () => {};
+  const { mutate, isLoading, errorData, isError } = useSignupMutation(form);
 
   return (
     <Form
-      submitForm={handleSubmit}
+      submitForm={mutate}
       className="flex flex-col w-full max-w-md gap-4 my-6"
     >
       <Input
@@ -28,6 +29,7 @@ export default function Signup() {
         placeholder="Informe seu nome"
         value={form.name}
         onChange={handleInputChange}
+        disabled={isLoading}
       />
       <Input
         id="email"
@@ -38,6 +40,7 @@ export default function Signup() {
         placeholder="Informe seu e-mail"
         value={form.email}
         onChange={handleInputChange}
+        disabled={isLoading}
       />
       <Input
         id="password"
@@ -48,18 +51,22 @@ export default function Signup() {
         placeholder="Informe sua senha"
         value={form.password}
         onChange={handleInputChange}
+        disabled={isLoading}
       />
       <Input
-        id="confirmPassword"
-        name="confirmPassword"
+        id="passwordConfirmation"
+        name="passwordConfirmation"
         label="Confirmação de senha:"
         type="password"
         icon="password"
         placeholder="Confirme sua senha"
-        value={form.confirmPassword}
+        value={form.passwordConfirmation}
         onChange={handleInputChange}
+        disabled={isLoading}
       />
-      <Button aria-label="Regigstrar usuário">Registrar</Button>
+      <Button aria-label="Regigstrar usuário" isLoading={isLoading}>
+        Registrar
+      </Button>
     </Form>
   );
 }
