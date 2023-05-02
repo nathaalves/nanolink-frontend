3c8f44ca2351e11198a7d285985bc954ffe79f8c
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-05-02 19:58:34 -0300

    feat: add react query dev tools

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 
 type QueryProviderPropsType = {
@@ -11,6 +12,9 @@ export default function QueryProvider({ children }: QueryProviderPropsType) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
