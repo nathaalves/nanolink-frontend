c6763b8464f9756dbe4786590416d3bf73dab67b
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-05-01 23:04:13 -0300

    style: add disabled style

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,10 +5,12 @@ import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import { IoMdMail, IoMdLock } from 'react-icons/io';
 import { BsFillPersonFill } from 'react-icons/bs';
 
+type IconType = 'email' | 'password' | 'name';
+
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   label?: string;
   labelVisible?: boolean;
-  icon?: 'email' | 'password' | 'name';
+  icon?: IconType;
 };
 
 export function Input({
@@ -16,6 +18,7 @@ export function Input({
   labelVisible = true,
   type = 'text',
   icon,
+  disabled,
   ...rest
 }: InputProps) {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -27,9 +30,9 @@ export function Input({
     }
   };
 
-  const icocnStyle = 'text-2xl text-zinc-600';
+  const icocnStyle = `text-2xl ${disabled ? 'text-zinc-400' : 'text-zinc-600'}`;
 
-  const icons = (name: string) => {
+  const Icons = (name: IconType) => {
     switch (name) {
       case 'email':
         return <IoMdMail className={icocnStyle} />;
@@ -37,23 +40,33 @@ export function Input({
         return <IoMdLock className={icocnStyle} />;
       case 'name':
         return <BsFillPersonFill className={icocnStyle} />;
+      default:
+        return <></>;
     }
   };
 
   return (
-    <div className="flex flex-col gap-1" onClick={handleClick}>
+    <div className={`flex flex-col gap-1`} onClick={handleClick}>
       {labelVisible && (
         <label htmlFor={rest.id} className="text-slate-700">
           {label}
         </label>
       )}
-      <div className="flex items-center h-12 px-2 gap-2 border rounded bg-white focus-within:border-sky-600 relative">
-        {icon && icons(icon)}
+      <div
+        className={`
+        flex items-center h-12 px-2 gap-2 border rounded bg-white focus-within:border-sky-600 relative
+        ${disabled ? 'cursor-default bg-gray-100' : 'cursor-text'}
+      `}
+      >
+        {icon && Icons(icon)}
         <input
           aria-label={label}
-          className={'flex-1 h-max outline-none truncate'}
+          className={
+            'flex-1 h-max outline-none truncate disabled:bg-gray-100 disabled:text-zinc-400'
+          }
           type={type === 'password' ? (isVisible ? 'text' : 'password') : type}
           ref={inputRef}
+          disabled={disabled}
           {...rest}
         />
         {type === 'password' && (
