faafff903bb88ed7831d73b1c834ce08cc0743e4
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-05-05 19:53:13 -0300

    style: update style

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 
 type NavBarPropsType = {
   children: React.ReactNode;
+  closed?: boolean;
 };
 
-export function NavBar({ children }: NavBarPropsType) {
+export function NavBar({ children, closed }: NavBarPropsType) {
   return (
-    <nav className="h-creen">
-      <ul className="flex flex-col gap-2">{children}</ul>
+    <nav
+      className={`
+        h-full pt-4 group/nav
+        ${closed ? 'max-w-min closed' : 'w-60'}
+    `}
+    >
+      <ul className="flex flex-1 flex-col gap-2">{children}</ul>
     </nav>
   );
 }
