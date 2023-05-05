d46683149c5e11c99537c944b94164ea9cd73ce8
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-05-04 22:57:39 -0300

    feat: update NavAnchor component

--- src/components/NavAnchor.tsx
+++ src/components/NavAnchor.tsx
@@ -1,30 +1,71 @@
+'use client';
+
 import Link from 'next/link';
-import { IconType } from 'react-icons';
+import { usePathname } from 'next/navigation';
+import { TbLink } from 'react-icons/tb';
 
 type NavAnchorPropsType = {
   path: string;
-  Icon?: IconType;
+  icon: string;
   label: string;
-  isActive?: boolean;
+  closed?: boolean;
 };
 
-export function NavAnchor({ path, Icon, label, isActive }: NavAnchorPropsType) {
+type IconPropsType = {
+  name: string;
+  className: string;
+};
+
+const Icon = ({ name, className }: IconPropsType) => {
+  const selectedIcon = () => {
+    switch (name) {
+      case 'link':
+        return <TbLink />;
+      default:
+        return null;
+    }
+  };
+
+  return <span className={className}>{selectedIcon()}</span>;
+};
+
+export function NavAnchor({ path, icon, label, closed }: NavAnchorPropsType) {
+  const activePath = usePathname();
+
   return (
     <li>
       <Link
         href={path}
         className={`
-          flex flex-1 items-center font-semibold px-2 gap-2 border-8 border-transparent rounded-r-lg hover:bg-slate-100
-          ${isActive ? 'text-sky-800 bg-sky-50 border-l-sky-800' : ''}
+          group flex items-center h-10 px-8 rounded-lg hover:bg-slate-100 relative
+          ${
+            activePath === path
+              ? 'text-sky-800 bg-sky-50 font-bold'
+              : 'text-zinc-800 font-semibold'
+          }
+          ${closed ? 'max-w-min' : 'w-60'}
         `}
       >
-        {Icon ? (
-          <>
-            <Icon className="text-2xl" />
-            {label}
-          </>
-        ) : (
-          label
+        <Icon name={icon} className="text-3xl" />
+        <span
+          className={`
+          items-center h-full px-4 whitespace-nowrap rounded-full
+          ${
+            closed
+              ? 'hidden group-hover:flex text-white absolute translate-x-full right-4 z-10 bg-sky-800'
+              : 'flex relative translate-x-0'
+          }
+        `}
+        >
+          {label}
+        </span>
+        {activePath === path && (
+          <span
+            className={`
+            w-2 h-full rounded-l-lg absolute right-0 bg-sky-800
+            ${closed ? 'group-hover:hidden' : 'group-hover:flex'}
+          `}
+          ></span>
         )}
       </Link>
     </li>
