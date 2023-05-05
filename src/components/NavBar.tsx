7ed588abb2747210c06f643c0e681c6edd293461
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-05-04 22:57:24 -0300

    feat: update NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
+import React from 'react';
+
 type NavBarPropsType = {
   children: React.ReactNode;
 };
 
 export function NavBar({ children }: NavBarPropsType) {
   return (
-    <nav className="w-60 h-creen">
-      <ul className="flex flex-col p-4 gap-2">{children}</ul>
+    <nav className="h-creen">
+      <ul className="flex flex-col gap-2">{children}</ul>
     </nav>
   );
 }
