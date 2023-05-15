db767e7e5115f8730a586f41a838d5602d10f638
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-05-15 16:40:53 -0300

    feat: add custom classes

--- src/components/LoadSpinner.tsx
+++ src/components/LoadSpinner.tsx
@@ -1,9 +1,29 @@
-export function LoadSpinner() {
+type LoadSpinnerPropsType = {
+  conatinerClass?: string;
+  spinnerClass?: string;
+};
+
+export function LoadSpinner({
+  conatinerClass,
+  spinnerClass,
+}: LoadSpinnerPropsType) {
   return (
-    <div className="flex items-center gap-2 h-3/4">
-      <div className="w-3 bg-white animate-load-spinner animation-delay-0"></div>
-      <div className="w-3 bg-white animate-load-spinner animation-delay-150"></div>
-      <div className="w-3 bg-white animate-load-spinner animation-delay-300"></div>
+    <div className={`flex items-center gap-2 h-3/4 ${conatinerClass}`}>
+      <div
+        className={`w-3 animate-load-spinner animation-delay-0 ${
+          spinnerClass ? spinnerClass : 'bg-white'
+        }`}
+      ></div>
+      <div
+        className={`w-3 animate-load-spinner animation-delay-150 ${
+          spinnerClass ? spinnerClass : 'bg-white'
+        }`}
+      ></div>
+      <div
+        className={`w-3 animate-load-spinner animation-delay-300 ${
+          spinnerClass ? spinnerClass : 'bg-white'
+        }`}
+      ></div>
     </div>
   );
 }
