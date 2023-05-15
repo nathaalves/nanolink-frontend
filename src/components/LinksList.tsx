15f4ebc85c270006457a81ceb83b06134d3cb8fe
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-05-15 16:42:39 -0300

    feat: create LinksList component

---
+++ src/components/LinksList.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+import linkImage from '@/assets/images/nanolink-192x192.png';
+import { useGetNanoLinksQuery } from '@/hooks/queries/useGetNanoLinksQuery';
+import { LoadSpinner } from './LoadSpinner';
+
+const BASE_URL = new URL(process.env.NEXT_PUBLIC_API_BASE_URL as string).host;
+
+export function LinksList() {
+  const { responseData: nanoLinks, isLoading } = useGetNanoLinksQuery();
+
+  return (
+    <>
+      {isLoading ? (
+        <span className="flex items-center justify-center w-40 h-16 m-auto">
+          <LoadSpinner spinnerClass="bg-sky-800" />
+        </span>
+      ) : nanoLinks && nanoLinks.length > 0 ? (
+        <ul>
+          {nanoLinks.map((nanoLink) => (
+            <li key={nanoLink.id}>
+              <Link
+                href={`detalhes/${nanoLink.nanoId}`}
+                className="flex p-4 border-t-[1px] border-slate-200 bg-white hover:bg-slate-50 relative"
+              >
+                <Image
+                  src={nanoLink.image ? nanoLink.image : linkImage}
+                  alt={nanoLink.title}
+                  width={54}
+                  height={54}
+                />
+                <div className="flex flex-col justify-evenly w-full ml-4">
+                  <span className="font-medium truncate">{nanoLink.title}</span>
+                  <p className="text-sm text-sky-600 font-bold truncate">
+                    {BASE_URL}/{nanoLink.nanoId}
+                  </p>
+                  <p className="text-xs text-zinc-600">{'05/05/2023'}</p>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <span className="flex flex-1 items-center justify-center text-zinc-500 font-medium">
+          Comece criando um Nano Link!
+        </span>
+      )}
+    </>
+  );
+}
