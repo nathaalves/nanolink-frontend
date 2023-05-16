49bf282be3e3ff069004bbac7a4f0a3c3b842650
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-05-16 16:13:06 -0300

    style: adds fix height

--- src/app/(painel)/nanolinks/(links)/layout.tsx
+++ src/app/(painel)/nanolinks/(links)/layout.tsx
@@ -11,7 +11,7 @@ export default function LinksLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col h-full pb-4">
+    <div className="flex flex-col h-screen max-h-screen pb-4">
       <header className="flex justify-between p-4 rounded-b-lg shadow-[0_0_10px_0_rgba(0,0,0,0.1)] bg-white">
         <div>
           <h1 className="text-4xl text-sky-800 font-bold my-6">Nano Links</h1>
@@ -26,7 +26,7 @@ export default function LinksLayout({
           <span className="hidden md:flex">Novo Link</span>
         </Anchor>
       </header>
-      <main className="flex flex-1 mt-4">{children}</main>
+      <main className="flex h-[calc(100vh-152px)] mt-4">{children}</main>
     </div>
   );
 }
