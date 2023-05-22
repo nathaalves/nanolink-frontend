17852971d21288d04b9ae5a0540bf095e62a97a9
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-05-22 16:42:57 -0300

    refactor: remove NavAnchor

--- src/templates/Dashboard.tsx
+++ src/templates/Dashboard.tsx
@@ -1,9 +1,4 @@
-'use client';
-
-import { NavAnchor } from '@/components/NavAnchor';
 import { NavBar } from '@/components/NavBar';
-import { BiLinkAlt } from 'react-icons/bi';
-import { MdQueryStats } from 'react-icons/md';
 
 type DashboardPropsType = {
   children: React.ReactNode;
@@ -12,14 +7,7 @@ type DashboardPropsType = {
 export function Dashboard({ children }: DashboardPropsType) {
   return (
     <main className="flex h-screen">
-      <NavBar closed={true}>
-        <NavAnchor
-          path="/estatisticas"
-          label="Estatísticas"
-          Icon={MdQueryStats}
-        />
-        <NavAnchor path="/nanolinks" label="Nano Links" Icon={BiLinkAlt} />
-      </NavBar>
+      <NavBar closed={true} />
       <div className="flex-1 bg-slate-100 px-8">{children}</div>
     </main>
   );
